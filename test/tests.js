f4509790577b691138f394cd7c23b82ce7c89ad8
Jack Baron <jackmbaron@gmail.com>
2018-08-14 17:30:44 +0100

    Initial tests

---
+++ test/tests.js
@@ -0,0 +1,18 @@
+// Test Suites
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+
+// Local Package
+const { analyze, generate } = require('../src/index.js')
+
+describe('analyse()', () => {
+  it('should be a function', () => {
+    expect(analyze).to.be.a('function')
+  })
+})
+
+describe('generate()', () => {
+  it('should be a function', () => {
+    expect(generate).to.be.a('function')
+  })
+})
